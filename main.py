2fad3b23781ff0b9a12cef2641eeb3cec175d4d6
EveryEunbin <116586151+EveryEunbin@users.noreply.github.com>
2022-11-19 16:28:35 +0000

    update main.py

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.core.utils import ChromeType
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
+import chromedriver_autoinstaller
+chromedriver_autoinstaller.install()
 
-chrome_service = Service(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install())
+# chrome_service = Service(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install())
 
 chrome_options = Options()
 options = [
@@ -19,7 +21,8 @@ options = [
 for option in options:
     chrome_options.add_argument(option)
 
-driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
+# driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
+driver = webdriver.Chrome(options=chrome_options)
 
 driver.get('http://google.com')
 print(driver.title)
